24ba64c92ad8388a25b2f8501483612995210208
David <ddude2410@live.com>
2020-12-04 22:50:40 -0500

    corrected directory traversing to file

--- 1/expenseAudit.py
+++ 1/expenseAudit.py
@@ -1,9 +1,11 @@
 #takes in a .txt file called 'expenses.txt' that is organized with one number per line
 #remove new line and checks which numbers add to 2020 and returns those
 #numbers multiplied together
+import os
+import sys
 
 numToSumTo = 2020
-expenses = [int(lines.strip()) for lines in open('expenses.txt', 'r')]
+expenses = [int(lines.strip()) for lines in open(os.path.join(sys.path[0], 'expenses.txt'), 'r')]
 
 val2 = {x*y for x in expenses for y in expenses if x!=y and x+y==numToSumTo}
 print(str(val2).strip('{}'))
